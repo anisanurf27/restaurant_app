35f44e5c93f5de2f07f0fc0d464809552f29f883
Zebedeus12 <nabillazebedeus@gmail.com>
2024-06-20 22:57:39 +0700

    menghilangkan sign up di user

--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -616,10 +616,6 @@
                     <div class="col-lg-3 col-md-6">
                         <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Newsletter</h4>
                         <p>Silahkan Hubungi Kami</p>
-                        <div class="position-relative mx-auto" style="max-width: 400px;">
-                            <input class="form-control border-primary w-100 py-3 ps-4 pe-5" type="text"
-                                placeholder="Your email">
-                        </div>
                     </div>
                 </div>
             </div>
